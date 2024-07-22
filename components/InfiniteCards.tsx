c75e64d86a4bc0fa825a3c8a6237826d8d879c1c
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-07-23 00:54:58 +0530

    fix err

--- components/InfiniteCards.tsx
+++ components/InfiniteCards.tsx
@@ -18,22 +18,27 @@ export function InfiniteMovingCardsHome() {
 
 const testimonials = [
   {
+    quote: "It was the best of times, it was the worst of times.",
     name: "Charles Dickens",
     title: "A Tale of Two Cities",
   },
   {
+    quote: "To be, or not to be: that is the question.",
     name: "William Shakespeare",
     title: "Hamlet",
   },
   {
+    quote: "All that we see or seem is but a dream within a dream.",
     name: "Edgar Allan Poe",
     title: "A Dream Within a Dream",
   },
   {
+    quote: "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
     name: "Jane Austen",
     title: "Pride and Prejudice",
   },
   {
+    quote: "Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to sea as soon as I can.",
     name: "Herman Melville",
     title: "Moby-Dick",
   },
