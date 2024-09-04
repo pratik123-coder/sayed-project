12add6e9f90ae38d9f6e72784fa2e3ce0460f53d
Hawsgard123 <himanshunayak2003@gmail.com>
2024-09-04 20:55:41 +0530

    good job done by me

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default function Home() {
       <TeamManagement /> */}
       <Roles />
       <FAQ />
-      <ReadyToGetStarted />
       {/* <Footer /> */}
     </div>
   );
 }
+
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,33 +5,103 @@ import logo from "../public/logo.png";
 const Navbar = () => {
   return (
     <header className="fixed shadow-lg z-50 flex flex-wrap sm:justify-start sm:flex-nowrap w-full bg-white text-sm py-4 dark:bg-neutral-800">
-      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
-        <div className="flex items-center justify-between">
-          <a className="flex-none text-xl font-semibold dark:text-white flex gap-x-4 justify-center items-center" href="/">
+      <nav
+        className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between"
+        aria-label="Global"
+      >
+        <div className="flex items-center justify-between gap-x-8">
+          <a
+            className="flex-none text-xl font-semibold dark:text-white flex gap-x-4 justify-center items-center"
+            href="/"
+          >
             <Image src={logo} alt="logo" width={150} height={150} />
-            
-            <Link className="font-medium text-blue-500" href="/" aria-current="page">Home</Link>
-            <Link className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" href="/about">About Us</Link>
-            <Link className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" href="/pricing">Pricing</Link>
-            <Link className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500" href="#">Work</Link>
-
           </a>
+          <div className="flex-none text-xl font-semibold dark:text-white flex gap-x-4 justify-center items-center">
+            <Link
+              className="font-medium text-blue-500"
+              href="/"
+              aria-current="page"
+            >
+              Home
+            </Link>
+            <Link
+              className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500"
+              href="/about"
+            >
+              About Us
+            </Link>
+            <Link
+              className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500"
+              href="/pricing"
+            >
+              Pricing
+            </Link>
+            <Link
+              className="font-medium text-gray-600 hover:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500"
+              href="#"
+            >
+              Work
+            </Link>
+          </div>
+
           <div className="sm:hidden">
-            <button type="button" className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10" data-hs-collapse="#navbar-with-collapse" aria-controls="navbar-with-collapse" aria-label="Toggle navigation">
-              <svg className="hs-collapse-open:hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
-              <svg className="hs-collapse-open:block hidden flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg>
+            <button
+              type="button"
+              className="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10"
+              data-hs-collapse="#navbar-with-collapse"
+              aria-controls="navbar-with-collapse"
+              aria-label="Toggle navigation"
+            >
+              <svg
+                className="hs-collapse-open:hidden flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="3" x2="21" y1="6" y2="6" />
+                <line x1="3" x2="21" y1="12" y2="12" />
+                <line x1="3" x2="21" y1="18" y2="18" />
+              </svg>
+              <svg
+                className="hs-collapse-open:block hidden flex-shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
             </button>
           </div>
         </div>
-        <div id="navbar-with-collapse" className="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block">
+        <div
+          id="navbar-with-collapse"
+          className="hidden transition-all duration-[0.1ms] overflow-hidden basis-full grow sm:block"
+        >
           <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
-            
-            <Link className="font-medium text-gray-600 hover:text-gray-400 text-xl dark:text-neutral-400 dark:hover:text-neutral-500" href="#">Raise Query</Link>
+            <Link
+              className="font-medium text-gray-600 hover:text-gray-400 text-xl dark:text-neutral-400 dark:hover:text-neutral-500"
+              href="#"
+            >
+              Raise Query
+            </Link>
           </div>
         </div>
       </nav>
     </header>
   );
-}
+};
 
 export default Navbar;
